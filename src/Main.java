df183a7bd4aae94fd4eaea3dfe7194d12ce067c5
shajn786 <shajn786@gmail.com>
2022-11-23 14:39:29 +0530

    addstudent

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
+
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+
+    static int rollno,adno,opt;
+    static String name,college;
+    static ArrayList<Student> list=new ArrayList<Student>();
+
+    static Scanner sc= new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        while(true) {
+
+            System.out.println("Employee App");
+            System.out.println("1 : Add Student\n"
+            );
+            opt = sc.nextInt();
+
+            switch (opt) {
+                case 1:
+                    System.out.println("enter the roll no");
+                    rollno = sc.nextInt();
+                    System.out.println("enter the name");
+                    name = sc.next();
+                    System.out.println("enter the admission number");
+                    adno = sc.nextInt();
+                    System.out.println("enter the college name");
+                    college = sc.next();
+
+                    list.add(new Student(rollno,adno,name,college));
+                    System.out.println("SUCCESSFULLY ADDED");
+                    break;
+
+            }
+        }
     }
 }
